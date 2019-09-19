5b507cb2ea5e161aa4000657f6e8f1c264a0078e
Roger Vilà <roger.vila.camon@hp.com>
2019-09-19 13:12:09 +0200

    wip

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php $content = 'test'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+<body>
+    <h1>Test</h1>
+    <hr>
+    <?php echo $content; ?>
+</body>
+</html>
